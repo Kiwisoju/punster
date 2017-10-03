61d83eac5b393286cacca9f015b65dea00c4d95c
kiwisoju <glennjamesforrest@gmail.com>
2017-10-03 19:18:10 +0800

    No jokes message

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,30 +8,34 @@
                 <div class="panel-body">
                     <h1>{{ str_plural($user->name) }} Jokes</h1>
 
-                    <table class="table table-striped">
-                        <thead>
-                            <th>Date</th>
-                            <th>Notes</th>
-                            <th>Referred by</th>
-                            <th>Points</th>
-                        </thead>
-                        <tbody>
-                            @foreach ($jokes as $joke)
+                    @if ($jokes->count())
+                        <table class="table table-striped">
+                            <thead>
+                                <th>Date</th>
+                                <th>Notes</th>
+                                <th>Referred by</th>
+                                <th>Points</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($jokes as $joke)
+                                    <tr>
+                                        <td>{{ $joke->created_at->addHours(8)->format('d-m-Y h:ia') }}</td>
+                                        <td>{{ $joke->notes }}</td>
+                                        <td>{{ $joke->referrer->name }}</td>
+                                        <td>{{ $joke->points }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                            <tfoot>
                                 <tr>
-                                    <td>{{ $joke->created_at->addHours(8)->format('d-m-Y h:ia') }}</td>
-                                    <td>{{ $joke->notes }}</td>
-                                    <td>{{ $joke->referrer->name }}</td>
-                                    <td>{{ $joke->points }}</td>
+                                    <th colspan="3">Total</th>
+                                    <th>{{ $jokes->sum('points') }}</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th colspan="3">Total</th>
-                                <th>{{ $jokes->sum('points') }}</th>
-                            </tr>
-                        </tfoot>
-                    </table>
+                            </tfoot>
+                        </table>
+                    @else
+                        <p>No jokes? This person isn't funny.</p>
+                    @endif
                 </div>
             </div>
         </div>
